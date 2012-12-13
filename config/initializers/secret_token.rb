2183c9355838a28b1928bcc2e70241e5260a53f7
Ryan Mulligan <rmulligan@tracell.com>
2012-12-13 12:28:54 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessageBoard::Application.config.secret_token = '2c2c12d940504d2d3d7885f11ffea2601e70084973a06b0f96adadb2fe114e0d3251011817da6f7f083443a0bf158d3b0f9b7ab4f1dbcf476d128c5a74ca3c9b'
